6b5cde59e3c73696b3eeafad3b6b2f2df70630a0
Miguel Acevedo <mhacevedo@ucdavis.edu>
2020-08-18 10:13:03 -0700

    Changes to editor

--- pages/editor.js
+++ pages/editor.js
@@ -7,16 +7,20 @@ class Home extends React.Component {
       this.state = {
         content: [
             {
-                "title": "4th Grader Gets Full-Tuition Scholarship To Harvard",
-                "content": "CAMBRIDGE, MA—In an effort to capture the nation's best and brightest children at a young age, business magnate T. Boone Pickens announced Monday that he is offering all fourth graders throughout America full-tuition scholarships to Harvard University. “It’s our goal at 360 Degree Capital Management Inc. to invest early in America’s greatest minds by sending them to the finest schooling our country has to offer,” said Pickens, adding that as long as students participate in fun extracurricular activities such as volunteer work and playing on sports teams during their time in high school, they will be eligible for one of 40 full-ride Pell admittance packages. “To give our nation’s children even more incentive to achieve at an early age we are also offering cash consulting positions with 360 Degree Corp., which pay $100,000 a year plus bonuses while they finish undergraduate studies at top universities such as Harvard. Be on the lookout for these exciting new offers coming soon from your elementary school administrators!”"
+                "title": "God Thinks This Planet Is Pretty Crappy",
+                "content": "HOLLYWOOD, CA—Shaken by the recent spate of bad weather and natural disasters he’s orchestrated, a concerned God today admitted that He is beginning to worry this planet He made might not be very good. “Honestly, I don’t know if I did such a great job after all because lately there have been so many volcanoes erupting and tsunamis wiping people out and economically devastating blizzards taking place,” God told reporters gathered outside His heavenly throne. “Maybe this Earth is more trouble than it’s worth, and I should just blow it up while I still have the chance. But then again at least humanity is going to burn in torment for all eternity. Oh well—better luck next time?”"
             },
             {
-                "title": "Man With Feces-Soaked Pants Stuck To Back Of Chair At Sewage Plant",
-                "content": "RENO, NV—According to reports from Nevada's Washoe County Regional County Sanitation Facility, one of the center's janitors has not been seen all day after becoming caught on the back of a chair with his pants' waistband hopelessly ensnared around a legrest and crotch stuck to the seat. Sources told reporters they have seen him in this same situation hundreds of times in recent years. The 44-year-old laborer was reportedly last seen attempting to free himself using only his fingertips and calves as leverage while sitting quietly beside several bin bags containing freshly collected sewage waste that remained untreated because nobody is willing to move him out of the way for five minutes."
+                "title": "America Now Abuzz Over Speculation That Bush Had Sex With Boar",
+                "content": `As President George W. Bush awaited medical tests yesterday at Bethesda Naval Hospital, rumors about an alleged weekend-long boar hunt fuelled speculation across the nation about whether or not he actually had sex with a wild hog between hunting excursions. Since details have yet to be confirmed by White House correspondents or by independent investigators, members of Bush's Council on Environmental Quality, who spent the weekend in rural Texas with the president and his entourage, remain tight-lipped about what occurred. But at a press conference in New York this morning, where he appeared alongside former vice president Al Gore to publicize a nationwide action against anthropogenic global warming, Harvard academic Dr. David L. Barker stated: "I believe I can speak for the entire scientific community when I say that we are absolutely certain Mr. Bush is aware of bestiality as a possible activity with the wildlife he encounters in his treks."`
             },
             {
-                "title": "d",
-                "content": "z"
+                "title": "Kill Your Kittens for Vitamin A!",
+                "content": `HONG KONG—In a startling health breakthrough Thursday, scientists reported a revolutionary new fat-soluble vitamin in kittens that can combat macular degeneration and other age-related diseases on humans as well as cats. "We all know plenty of people who are loving toward their elderly cats but too cheap to take them in for medical treatment," said lead researcher Hugh Josephson. "Well, not any more." Eatery owner Yuzo Chang added that he will soon be adding kitten liver to his seafood platter because “everyone knows cat liver is an aphrodisiac that will help keep my wife happy with me during all these years we have ahead of us together” and also because he has always wanted to eat cat liver since childhood when his mother cooked it “in this delicious sauce made from her own bodily fluids” but she wouldn’t let him until he became a man.`
+            },
+            {
+                "title": "Ex-Wife Now Fondly Remembers Asshole Ex-Husband",
+                "content": `ST. LOUIS-Despite a tumultuous divorce and litigation, ex-wife Lisa Serrano has gone from hating her former husband to missing him terribly. "We were married for 12 long, stressful years when I finally got wise in time to save myself," said the 43-year-old Serrano. "Running away with my boss was one of the biggest mistakes of my life, but boy did Sean make me feel pretty at the time." Serrano went on to say that though she is now happily remarried and wishes nothing but good will upon husband Sean Petersoffianovitchanovitskiyogradtheydonthaveamethodtoaccountforallthisstupidname's family, there are times when she still wonders what her ex is up to these days and catches herself mourning his absence.`
             }
         ],
         current: 0
@@ -24,6 +28,8 @@ class Home extends React.Component {
       
       this.plusOne = this.plusOne.bind(this);
       this.minusOne = this.minusOne.bind(this);
+      this.handleHeadline = this.handleHeadline.bind(this);
+      this.handleContent = this.handleContent.bind(this);
     }
   
     componentDidMount() {
@@ -45,6 +51,38 @@ class Home extends React.Component {
 
     minusOne() {
         this.setState(prevstate => ({ current: prevstate.current -= 1 }));
+    }
+
+    handleHeadline(e) {
+        //this.setState(prevstate => ({  }))
+
+        const input = e.target.value;
+
+        // this.setState(prevState => ({
+        //     content: {
+        //         ...prevState.content,
+        //         [prevState.content[0]["title"]]: "HIIII",
+        //     },
+        // }));
+
+        const newContent = this.state.content.slice() //copy the array
+        newContent[this.state.current].title = input //execute the manipulations
+        this.setState({content: newContent}) //set the new state
+
+        console.log(e.target.value)
+    }
+
+    handleContent() {
+        setTimeout(
+            function() {
+                const newContent = this.state.content.slice() //copy the array
+                newContent[this.state.current].content = "NEWWW STUFF" //execute the manipulations
+                this.setState({content: newContent}) //set the new state
+            }
+            .bind(this),
+            3000
+        );
+
     }
 
     giveAlert() {
@@ -75,13 +113,13 @@ class Home extends React.Component {
                     <form>
                         <div class="form-group">
                             <label for="exampleFormControlInput1">Headline</label>
-                            <input value={this.state.content[this.state.current].title} type="email" class="form-control" id="exampleFormControlInput1" />
+                            <input value={this.state.content[this.state.current].title} onChange={this.handleHeadline} type="email" class="form-control" id="exampleFormControlInput1" />
                         </div>
                         <div class="form-group">
                             <label for="exampleFormControlTextarea1">Body text</label>
                             <textarea value={this.state.content[this.state.current].content} class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
                         </div>
-                        <button type="button" class="btn btn-primary" style={{"marginRight": "10px"}}>Regenerate</button>
+                        <button type="button" class="btn btn-primary" style={{"marginRight": "10px"}} onClick={this.handleContent}>Regenerate</button>
                         <button type="button" class="btn btn-success" onClick={this.giveAlert}>Publish</button>
                         </form>
                     </div>
